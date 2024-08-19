76e9d862283a60c7196e5a234cc9f5a46b4828aa
Nicky Chen <nickychen@Nickys-MacBook-Pro.local>
2024-08-18 19:47:27 -0700

    fixed errors

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -37,7 +37,6 @@ function Signup() {
   return (
     <div className='signup-container'> 
       <form onSubmit={handleSignup}>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
